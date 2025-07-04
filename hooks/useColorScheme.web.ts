294e0c217e045990bf5a148fc0c27dca54caf41a
gurneeshsinghcc <gurneesh@circuitstream.ca>
2025-07-04 03:16:30 +0000

    fixed theme change issue on web

--- hooks/useColorScheme.web.ts
+++ hooks/useColorScheme.web.ts
@@ -1,21 +1,29 @@
 import { useEffect, useState } from 'react';
-import { useColorScheme as useRNColorScheme } from 'react-native';
 
 /**
  * To support static rendering, this value needs to be re-calculated on the client side for web
  */
 export function useColorScheme() {
   const [hasHydrated, setHasHydrated] = useState(false);
+  const [scheme, setScheme] = useState<'light' | 'dark'>('light');
 
   useEffect(() => {
     setHasHydrated(true);
+
+    const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+
+    setScheme(mediaQuery.matches ? 'dark' : 'light');
+
+    const listener = (e: MediaQueryListEvent) => {
+      setScheme(e.matches ? 'dark' : 'light');
+    };
+
+    mediaQuery.addEventListener('change', listener);
+
+    return () => {
+      mediaQuery.removeEventListener('change', listener);
+    };
   }, []);
 
-  const colorScheme = useRNColorScheme();
-
-  if (hasHydrated) {
-    return colorScheme;
-  }
-
-  return 'light';
+  return hasHydrated ? scheme : 'light';
 }
